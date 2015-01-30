b2e45005cd9d396628d0e0cd4bc993b1b82c0637
tingilee <tingilee@gmail.com>
2015-01-30 10:22:59 -0800

    small modification for displaying and console logging

--- app.js
+++ app.js
@@ -11,95 +11,95 @@ var mongoPort = 27017; // Mongo port by default
 var PORT = 3000; // port of the server
 var collectionDriver;
 
-//var routes = require(./routes/index);
-//var users = require(./routes/users);
-
 var app = express();
 app.set('port', PORT); 
-app.set('views', path.join(__dirname, 'views')); //A
-app.set('view engine', 'jade'); //B
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'jade'); 
  
 // after the app.set lines, but before any app.use or app.get lines:
 // express now parses the incoming body data
 app.use(express.bodyParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
  
-var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); //B
-mongoClient.open(function(err, mongoClient) { //C
-  if (!mongoClient) {
-      console.error("Error! Exiting... Must start MongoDB first");
-      process.exit(1); //D
-  }
-  var db = mongoClient.db("MyDatabase");  //E
-  collectionDriver = new CollectionDriver(db); //F
+var mongoClient = new MongoClient(new Server(mongoHost, mongoPort)); 
+mongoClient.open(function(err, mongoClient) { 
+  	if (!mongoClient) {
+      	console.error("Error! Exiting... Must start MongoDB first");
+      	process.exit(1); //D
+ 	 }
+  	var db = mongoClient.db("MyDatabase");  
+  	collectionDriver = new CollectionDriver(db); 
 });
 
-// MongoDB collection
-// doesn't work when i add get before the /:collection? whyy
-app.get('/:collection', function(req, res) { //A
-   var params = req.params; //B
-   collectionDriver.findAll(req.params.collection, function(error, objs) { //C
-    	  if (error) { res.send(400, error); } //D
-	      else { 
-	      	
-	        if (req.accepts('html')) { //E
-    	        res.render('rawdata',{objects: objs, collection: req.params.collection}); //F
+// Get the entire collection (Table)
+app.get('/:collection', function(req, res) {
+   	var params = req.params; 
+   	collectionDriver.findAll(req.params.collection, function(error, objs) { 
+    	if (error) { 
+    		res.send(400, error); 
+    	} else { 
+	        if (req.accepts('html')) { 
+    	        res.render('data',{objects: objs, collection: req.params.collection}); //F
               	
-              } else {
-	          	res.set('Content-Type','application/json'); //G
-                res.send(200, objs); //H
-              }
-         }
+            } else {
+	          	res.set('Content-Type','application/json');
+                res.send(200, objs); 
+            }
+        }
    	});
 });
  
-app.get('/:collection/:entity', function(req, res) { //I
-   var params = req.params;
-   var entity = params.entity;
-   var collection = params.collection;
-   if (entity) {
-       collectionDriver.get(collection, entity, function(error, objs) { //J
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //K
+// Get specific item
+app.get('/:collection/:entity', function(req, res) { 
+   	var params = req.params;
+   	var entity = params.entity;
+   	var collection = params.collection;
+   	if (entity) {
+       	collectionDriver.get(collection, entity, function(error, objs) { 
+          	if (error) { res.send(400, error); }
+         	else { res.send(200, objs); }
        });
-   } else {
-      res.send(400, {error: 'bad url', url: req.url});
-   }
+   	} else {
+      	res.send(400, {error: 'bad url', url: req.url});
+   	}
 });
 
-app.post('/:collection', function(req, res) { //A
-    var object = req.body;
-    var collection = req.params.collection;
-    var newObj = {};
-    var longitude, latitude;
+function filterObjection(object) {
+	var newObj = {};
     // filter out the fields to store
     if (object.longitude && object.latitude) {
     	newObj.longitude = object.longitude;
     	newObj.latitude = object.latitude;
     	newObj.title = object.title;
+    	newObj.source = object.source;
+    	newObj.description = object.description;
     }
-    console.log(newObj);
-	     
+    return newObj;
+}
+
+// Add item to collection 
+app.post('/:collection', function(req, res) {
+    var object = req.body;
+    var collection = req.params.collection;
+    var newObj = filterObjection(object);
+    console.log(newObj + " is now added to collection " + collection);	// print Json
 
     collectionDriver.save(collection, newObj, function(err,docs) {
           if (err) { res.send(400, err); } 
-          else { res.send(201, docs); } //B
+          else { res.send(201, docs); }
      });
 });
 
-// this updates the item 
-app.put('/:collection/:entity', function(req, res) { //A
+// Update the item 
+app.put('/:collection/:entity', function(req, res) { 
     var params = req.params;
     var entity = params.entity;
     var collection = params.collection;
     if (entity) {
        collectionDriver.update(collection, req.body, entity, function(error, objs) { //B
           if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C
+          else { res.send(200, objs); }
        });
    } else {
        var error = { "message" : "Cannot PUT a whole collection" };
@@ -107,42 +107,21 @@ app.put('/:collection/:entity', function(req, res) { //A
    }
 });
 
-app.delete('/:collection/:entity', function(req, res) { //A
+// Delete the item from collection
+app.delete('/:collection/:entity', function(req, res) {
     var params = req.params;
     var entity = params.entity;
     var collection = params.collection;
     if (entity) {
-       collectionDriver.delete(collection, entity, function(error, objs) { //B
+       collectionDriver.delete(collection, entity, function(error, objs) {
           if (error) { res.send(400, error); }
-          else { res.send(200, objs); } //C 200 b/c includes the original doc
+          else { res.send(200, objs); } 
        });
    } else {
        var error = { "message" : "Cannot DELETE a whole collection" };
        res.send(400, error);
    }
 });
-
-/*
-app.get('/insert', function (req, res) {
-  res.send('<html><body><h1>Insert into table</h1></body></html>');
-});
-
-app.get('/get', function (req, res) {
-  res.send('<html><body><h1>Get from table</h1></body></html>');
-});
-
-app.post('/postinsert', function (req, res) {
-  res.send('<html><body><h1>HTTP POST: insert into table</h1></body></html>');
-});
-*/
-
-// :a? means wildcard. can be anything
-/*
-app.get('/:a?/:b?/:c?', function (req,res) {
-	res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
-});
-*/
-
 
 app.get('/', function (req, res) {
   res.send('<html><body><h1>Hello World</h1></body></html>');
