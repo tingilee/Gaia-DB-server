357aa925e8f77558e0ec520d3f4e07613da6476a
tingilee <tingilee@gmail.com>
2015-02-23 13:59:08 -0800

    fix temp.js to test cross origin

--- temp.js
+++ temp.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var path = require('path');
-var bodyParser = require('body-parser');
+var http = require('http');
+//var bodyParser = require('body-parser');
 
-
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
+var PORT = 4000; // port of the server
 
 //configuration =========================================================
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('port', PORT); 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(bodyParser({limit: '50mb'}));
+app.use(express.bodyParser());
+//app.use(bodyParser.urlencoded());
+//app.use(bodyParser({limit: '50mb'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
@@ -28,5 +29,18 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use('/', routes);
+//app.use('/', routes);
+
+app.get('/', function(req, res) {
+	console.log("cross-origin");
+  	res.send('<html><body><h1>Hello World</h1></body></html>');
+
+});
+
+app.use(function (req,res) {
+    res.render('404', {url:req.url});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'))});
 
