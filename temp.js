bcb5faed3c3297314cd1a13fc49cef61ad50d1cc
tingilee <tingilee@gmail.com>
2015-02-23 14:01:48 -0800

    fix a bug in view engine

--- temp.js
+++ temp.js
@@ -13,7 +13,7 @@ var PORT = 4000; // port of the server
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade'); 
 app.set('port', PORT); 
 
 app.use(express.bodyParser());
