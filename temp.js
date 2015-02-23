32a60615431eeb8b4f3441d7bbe8ae7db7b241a7
tingilee <tingilee@gmail.com>
2015-02-23 13:50:40 -0800

    test cross origin

--- temp.js
+++ temp.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var nodemailer = require('nodemailer')
 
 
-var mongo = require('mongoskin');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,11 +15,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser({limit: '50mb'}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
